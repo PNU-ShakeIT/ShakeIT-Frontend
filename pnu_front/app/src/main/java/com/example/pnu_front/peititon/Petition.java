dee1159c6e2b3f1a8e1ba878add39b8d3201c37b
ki-met-hoon <101192772+ki-met-hoon@users.noreply.github.com>
2022-11-05 00:10:25 +0900

    pull 전 머지 -세훈

--- pnu_front/app/src/main/java/com/example/pnu_front/peititon/Petition.java
+++ pnu_front/app/src/main/java/com/example/pnu_front/peititon/Petition.java
@@ -1,21 +1,12 @@
 package com.example.pnu_front.peititon;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.pnu_front.R;
-//import com.example.pnu_front.RetrofitManager.ApiClient;
-//import com.example.pnu_front.RetrofitManager.ApiInterface;
-//import com.example.pnu_front.RetrofitManager.PetitionModel;
-//import com.example.pnu_front.ViewManager.PetitionAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import RetrofitMananger.RetrofitInstance;
