0b80147cc5e3d3308c9066895e91800c4c69aac6
hyojun kim <guwns7732@gmail.com>
2022-11-06 01:36:41 +0900

    프론트 병합(11/5)

---
+++ pnu_front/app/src/main/java/com/example/pnu_front/lawmake.java
@@ -0,0 +1,42 @@
+package com.example.pnu_front;
+
+public class lawmake {
+
+        private String num;
+        private String title;
+        private String day;
+        private String user;
+
+        public lawmake(String title,String num,String user,String day){
+            this.title = title;
+            this.num = num;
+            this.user = user;
+            this.day = day;
+        }
+        public String getTitle(){
+            return this.title;
+        }
+        public String getNum(){
+            return  this.num;
+        }
+        public String getDay(){
+            return this.day;
+        }
+        public String getUser(){
+            return this.user;
+        }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+    public void setUser(String user){
+            this.user = user;
+    }
+    public void setDay(String day){
+            this.day = day;
+    }
+}
