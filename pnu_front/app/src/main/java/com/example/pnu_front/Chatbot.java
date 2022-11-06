b5be92b2d39db94538720f1497cb48e026a8be88
moyeongmin <qa98589858@gmail.com>
2022-11-06 23:14:58 +0900

    11 / 06 실시간 진행중 청원 구현(밑에거 빼고) / 일정 sorting 진행중 / 국회의원 세부내용 출력 진행중

---
+++ pnu_front/app/src/main/java/com/example/pnu_front/Chatbot.java
@@ -0,0 +1,41 @@
+package com.example.pnu_front;
+
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+public class Chatbot extends AppCompatActivity {
+    WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chatbot);
+
+        webView = findViewById(R.id.webView);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true); // allow the js
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON); //화면이 계속 켜짐
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_FULL_USER);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://dc03-58-72-42-91.jp.ngrok.io");
+    }
+}
